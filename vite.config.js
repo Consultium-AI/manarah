9c05b7360dbc6b685cb741e04880cca10cba33d4
Consultium-AI <info@consultiumai.nl>
2025-09-27 19:10:52 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(function (_a) {
+    var command = _a.command, mode = _a.mode;
+    var isBuild = command === 'build';
+    return {
+        plugins: [react()],
+        base: isBuild ? '/manarah/' : '/',
+    };
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const isBuild = command === 'build';
+  return {
+    plugins: [react()],
+    base: isBuild ? '/manarah/' : '/',
+  };
 });
 
 
